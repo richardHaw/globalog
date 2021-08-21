ad0ef25cc48a0c2103b1521cb61edc85b993894b
richardHaw <59139020+richardHaw@users.noreply.github.com>
2021-08-21 21:06:51 +0900

    writeSep
    open file implemented

--- tester.jsx
+++ tester.jsx
@@ -1,14 +1,15 @@
 // @include"source/globalog.jsxinc";
 // @include"source/widgets/globalog_win.jsxinc";
 
-var betlog = CreateGlobaLog("C:/Users/richa/Documents/xxx2.txt");
+
+var betlog = CreateGlobaLog(Folder.myDocuments.fsName.split("\\").join("/") + "/xxx.txt");
 betlog.log("ddd",3);
 betlog.log("");
 //~ betlog.print(true);
 
 betlog.log("isko",1);
 betlog.log("kalabaw",0);
-betlog.log(betlog.separator);
+betlog.writeSep();
 betlog.setLevel(4);
 betlog.log("palitaw");
 betlog.log("ppp");
