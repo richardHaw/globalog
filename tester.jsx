8bae7a9e2121e578065174f6dcaa1dc0831d2cfb
richardHaw <59139020+richardHaw@users.noreply.github.com>
2021-08-21 14:46:37 +0900

    first working sample

---
+++ tester.jsx
@@ -0,0 +1,18 @@
+// @include"source/globalog.jsxinc";
+
+
+var betlog = new GlobaLog();
+
+
+betlog.write("ddd",3);
+betlog.write("");
+betlog.print(true);
+
+betlog.write("isko",1);
+betlog.write("kalabaw",0);
+betlog.write(betlog.div);
+betlog.setLevel(4);
+betlog.write("palitaw");
+betlog.write("ppp");
+betlog.write("clit");
+
